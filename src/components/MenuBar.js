7b58d4d95d3dce3e7bff53e7572a5bbd68483aad
Philip Sterling <flatironschool@Flatirons-MacBook-Air-8.local>
2019-08-08 10:02:37 -0700

    Done.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
+import { Pokemon } from './Pages';
 
-const MenuBar = (props) => {
-
+class MenuBar extends React.Component {
   /*
 
   The 'a' tags below are the menu items. Think about the way a menu 
@@ -10,29 +10,55 @@ const MenuBar = (props) => {
   this programatically? What other behavior do we expect when we click
   on a menu item? Do we need state in this component, and if not, how can
   this component be made aware of what is currently the active menu item?
-
+  
   */
 
+  state = {
+    profile: "item active",
+    pokemon: "item",
+    cocktail: "item",
+    photo: "item"
+  }
+
+  handleChangeActive = (e) => {
+    let newActive = e.target.id
+    this.setState(prevState => {
+      for (const key in prevState) {
+        if (prevState.hasOwnProperty(key)) {
+          prevState[key] = "item"
+        }
+      }
+      prevState[newActive] = "item active"
+      return prevState
+    })
+  }
+
+  handleClick = (e) => {
+    this.handleChangeActive(e)
+    this.props.changePageState(e.target.id)
+  }
+
+  render(){
   return (
     <div className="ui four item menu">
-      <a className="item active" id="profile">
+      <a  onClick={this.handleClick} className={this.state.profile} id="profile">
         <i className="user large icon" id="profile"/>
       </a>
 
-      <a className="item" id="photo">
+      <a onClick={this.handleClick} className={this.state.photo} id="photo">
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className="item" id="cocktail">
+      <a onClick={this.handleClick} className={this.state.cocktail} id="cocktail">
         <i className="cocktail large icon" id="cocktail"/>
       </a>
 
-      <a className="item" id="pokemon"> 
+      <a onClick={this.handleClick} className={this.state.pokemon} id="pokemon"> 
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
   )
 
-}
+}}
 
 export default MenuBar
